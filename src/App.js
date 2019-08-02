b170b0ca56b4bc6f9639683dff8123495ad4ea7a
alelulu <ale.silvasuazo@gmail.com>
2019-08-02 01:19:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container window-content">
+        <div className="row header navbar navbar-fixed-top">
+          <h6 className="title"><img src="https://i.ibb.co/PTYD9dS/Cat.png"/> ALELULU - FRONT-END DEVELOPER</h6>
+        </div>
+        <div className="row body">
+          <div className="col-6">
+            <h5>div prueba</h5>
+          </div>
+          <div className="col-6">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
