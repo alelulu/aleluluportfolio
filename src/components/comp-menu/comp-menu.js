44503e1dc27acfd073ecef9406177a5617c91435
alelulu <ale.silvasuazo@gmail.com>
2019-08-27 22:06:48 -0400

    changing menu color

--- src/components/comp-menu/comp-menu.js
+++ src/components/comp-menu/comp-menu.js
@@ -11,13 +11,13 @@ class Menu extends Component{
       <div className="menu-container container">
         <div className="row nav-row">
           <div className="col-sm-4 col-md-2 col-lg-1 btn-container">
-            <Link to="/about"><Btn title={this.props.language.language.menu.about} src="https://i.ibb.co/RN54ZBH/home.png"/></Link>
+            <Link to="/about"><Btn title={this.props.language.language.menu.about} src="https://i.ibb.co/T1FsWfX/home.png"/></Link>
           </div>
           <div className="col-sm-4 col-md-2 col-lg-1 btn-container">
-            <Link to="/projects"><Btn title={this.props.language.language.menu.projects} src="https://i.ibb.co/1b0MX8j/projects.png"/></Link>
+            <Link to="/projects"><Btn title={this.props.language.language.menu.projects} src="https://i.ibb.co/tPLBD8r/projects.png"/></Link>
           </div>
           <div className="col-sm-4 col-md-2 col-lg-1 btn-container">
-            <Link to="/skills"><Btn title={this.props.language.language.menu.skills} src="https://i.ibb.co/37K3VK2/skills.png"/></Link>
+            <Link to="/skills"><Btn title={this.props.language.language.menu.skills} src="https://i.ibb.co/w7FSRsQ/skills.png"/></Link>
           </div>
         </div>
       </div>
